aab3fa56d0a4e84f2e961f3f1872f4255d7e758a
Indrajith Rambukkanage <pushpaka.rambukkanage@gmail.com>
2019-02-23 16:38:49 +1000

    Combined both algo execution in the results

--- NearestNeighbour2DAlgos/Program.cs
+++ NearestNeighbour2DAlgos/Program.cs
@@ -11,32 +11,30 @@ namespace NearestNeighbour2DAlgos
     {
         static int Main(string[] args)
         {
-            var noOfPoints = 4000000;
-            var pointsGenerator = new TestSpaceGenerator();
-            var points = pointsGenerator.GenerateTestSpace(noOfPoints, -2000, 2000, -2000, 2000);
+            var noOfPointsToFind = 3;
 
+            var pointsGenerator = new TestSpaceGenerator();
             var executor = new SearchExecutor();
 
-            WriteDataTableHeader("BruteForce Iteration", "BruteForce - results", "BruteForce - time");
+            var bruteForceSearcher = new BruteForceNearestNeighbourSearcher();
+            var optimisedBruteForceSearcher = new OptimisedBruteForceNearestNeighbourSearcher();
 
-            for (int i = 0; i < 10; i++)
+            WriteDataTableHeader("No. of Points In the Space", "No. of Neighbours to Find", "Given Point", "BruteForce - results", "BruteForce - time", "Optmised - results", "Optmised - time");
+            
+            for (int i = 1; i <= 10; i++)
             {
-                var randomIndex = new Random().Next(0, noOfPoints);
+                var currentNoOfPoints = i * 1000000;
+
+                var randomIndex = new Random().Next(0, currentNoOfPoints);
+
+                var points = pointsGenerator.GenerateTestSpace(currentNoOfPoints, -2000, 2000, -2000, 2000);
                 var point = points[randomIndex];
-                var result = executor.ExecuteSearch(new BruteForceNearestNeighbourSearcher(), point, 3, points);
 
-                WriteDataTableRow(ResultsToString(point), ResultsToString(result.NearestNeighbours), result.TimeInMilliseconds);
-            }
 
-            WriteDataTableHeader("OptimisedBruteForce Iteration", "BruteForce - results", "BruteForce - time");
+                var bruteForceResult = executor.ExecuteSearch(bruteForceSearcher, point, noOfPointsToFind, points);
+                var optimisedResult = executor.ExecuteSearch(optimisedBruteForceSearcher, point, noOfPointsToFind, points);
 
-            for (int i = 0; i < 10; i++)
-            {
-                var randomIndex = new Random().Next(0, noOfPoints);
-                var point = points[randomIndex];
-                var result = executor.ExecuteSearch(new OptimisedBruteForceNearestNeighbourSearcher(), point, 3, points);
-
-                WriteDataTableRow(ResultsToString(point), ResultsToString(result.NearestNeighbours), result.TimeInMilliseconds);
+                WriteDataTableRow(currentNoOfPoints, noOfPointsToFind, ResultsToString(point), ResultsToString(bruteForceResult.NearestNeighbours), bruteForceResult.TimeInMilliseconds, ResultsToString(optimisedResult.NearestNeighbours), optimisedResult.TimeInMilliseconds);
             }
 
             Console.WriteLine("Press enter to terminate");
@@ -57,15 +55,15 @@ namespace NearestNeighbour2DAlgos
             return String.Join(',', texts.ToArray());
         }
 
-        private static void WriteDataTableHeader(string header1, string header2, string header3)
+        private static void WriteDataTableHeader(string header1, string header2, string header3, string header4, string header5, string header6, string header7)
         {
-            Console.WriteLine($"|{header1}|{header2}|{header3}|");
-            Console.WriteLine($"|-----------|-----------|-----------|");
+            Console.WriteLine($"|{header1}|{header2}|{header3}|{header4}|{header5}|{header6}|{header7}|");
+            Console.WriteLine($"|-----------|-----------|-----------|-----------|-----------|-----------|-----------|");
         }
 
-        private static void WriteDataTableRow(string index, string bruteForceResults, double bruteForceTime)
+        private static void WriteDataTableRow(int noOfTotalPoints, int noOfPointsToFind, string givenPoint, string bruteForceResults, double bruteForceTime, string optmisedResults, double optmisedTime)
         {
-            Console.WriteLine($"|{index}|{bruteForceResults}|{bruteForceTime}|");
+            Console.WriteLine($"|{noOfTotalPoints}|{noOfPointsToFind}|{givenPoint}|{bruteForceResults}|{bruteForceTime}|{optmisedResults}|{optmisedTime}|");
         }
     }
 }
